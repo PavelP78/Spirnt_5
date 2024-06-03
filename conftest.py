49db46ab8fb87d5b8d921484fb53b7f58c6cbfe4
Pavel Pliasunov <paha_a_b@yandex.ru>
2024-06-03 18:31:15 +0300

    исправлено

--- conftest.py
+++ conftest.py
@@ -15,5 +15,5 @@ def delete_data_base():
     response = requests.post(f"{MainUrl.url}{EndPoint.verification_courier}", data=VerificationCourierLogin.login_courier)
     json_response = response.json()
     user_id = json_response['id']
-    requests.delete(f"{MainUrl.url}{EndPoint.delete_courier}{user_id}")
+    yield requests.delete(f"{MainUrl.url}{EndPoint.delete_courier}{user_id}")
     return delete_data_base
--- test/test_login_courier.py
+++ test/test_login_courier.py
@@ -4,6 +4,7 @@ from  logins import CourierLogin, VerificationCourierLogin
 from data import MainUrl, EndPoint
 from base.base_methods import HttpMethods
 
+
 @allure.title('Проверка:  курьер может авторизоваться')
 def test_verification_courier(courier):
     response = HttpMethods.verification_courier()
@@ -12,17 +13,17 @@ def test_verification_courier(courier):
 
 
 @allure.title('Проверка ошибки авторизациии без ввода логин')
-def test_verification_courier_without_login(courier):
+def test_verification_courier_without_login(courier, delete_data_base):
     response = requests.post(f"{MainUrl.url}/api/v1/courier/login", data=VerificationCourierLogin.without_login)
     assert response.status_code == 400
-    response.delete = HttpMethods.delete_courier()
+
 
 
 @allure.title('Проверка ошибки авторизациии без ввода пароля')
-def test_verification_courier_without_password(courier):
+def test_verification_courier_without_password(courier, delete_data_base):
     response = requests.post(f"{MainUrl.url}/api/v1/courier/login", data=VerificationCourierLogin.without_password)
     assert response.status_code == 400
-    response.delete = HttpMethods.delete_courier()
+
 
 
 @allure.title('Cистема вернёт ошибку, если неправильно указать пароль')
